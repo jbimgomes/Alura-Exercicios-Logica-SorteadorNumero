e9829b07db9fe960c943e4d4ba92dcd0e226477c
João Luis Bim Gomes <jbimgomes1@gmail.com>
2024-09-13 08:52:52 -0300

    Update app.js
    Validacao de dados incluida

--- app.js
+++ app.js
@@ -21,17 +21,22 @@ function sortear() {
   let numero;
   let sorteados = [];
 
-  for (let i = 0; i < quantidade; i++) {
-    numero = obterNumeroAleatorio(de, ate);
-
-    while (sorteados.includes(numero)) {
+  if (validarDados()) {
+    for (let i = 0; i < quantidade; i++) {
       numero = obterNumeroAleatorio(de, ate);
-    }
 
-    sorteados.push(numero);
+      while (sorteados.includes(numero)) {
+        numero = obterNumeroAleatorio(de, ate);
+      }
+
+      sorteados.push(numero);
+    }
+    exibeNaTelaSorteados(sorteados);
+    alterarStatusBotao();
+  } else {
+    alterarStatusBotao();
+    reiniciar();
   }
-  exibeNaTelaSorteados(sorteados);
-  alterarStatusBotao();
 }
 
 function obterNumeroAleatorio(min, max) {
@@ -44,6 +49,26 @@ function reiniciar() {
   document.getElementById("ate").value = "";
   document.getElementById(
     "resultado"
-  ).innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora.</label>`;
+  ).innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
   alterarStatusBotao();
+}
+
+function validarDados() {
+  let quantidade = parseInt(document.getElementById("quantidade").value);
+  let de = parseInt(document.getElementById("de").value);
+  let ate = parseInt(document.getElementById("ate").value);
+
+  if (quantidade <= 0) {
+    alert("Quantidade não pode ser igual ou menor que zero!");
+    return false;
+  } else {
+    if (de >= ate) {
+      alert(
+        "Dados incorretos, verifique o seu intervalo de números (de não pode ser igual ou maior que o até"
+      );
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
